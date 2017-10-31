7d090b1059260275d6ac3166d3627ceba009908e
KayleeRoxanne <33269494+KayleeRoxanne@users.noreply.github.com>
2017-11-01 00:57:52 +0100

    Add files via upload

---
+++ js/web.js
@@ -0,0 +1,21 @@
+var search = document.querySelector('.search');
+search.addEventListener('click', function () {
+    search.classList.toggle('active')
+});
+
+var favorite = document.querySelector('article header button');
+favorite.addEventListener('click', function () {
+    favorite.classList.toggle('active')
+});
+
+/*var download = document.querySelector('.downloaden');
+download.addEventListener('click', function () {
+    download.classList.toggle('active')
+});*/
+
+var downloads = document.querySelectorAll('.downloaden');
+for (var i = 0; i < downloads.length; i++) {
+    downloads[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+    })
+}
