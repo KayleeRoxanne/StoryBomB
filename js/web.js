24b27af46bc0eec22dcbf51c9a0c0bd86a8df657
KayleeRoxanne <33269494+KayleeRoxanne@users.noreply.github.com>
2017-11-08 13:36:24 +0100

    Add files via upload

--- js/web.js
+++ js/web.js
@@ -3,14 +3,23 @@ search.addEventListener('click', function () {
     search.classList.toggle('active')
 });
 
-var favorite = document.querySelector('article header button');
-favorite.addEventListener('click', function () {
-    favorite.classList.toggle('active')
-});
+var number = 0;
+var favorite = document.querySelectorAll('article header button');
+for (var i = 0; i < favorite.length; i++) {
+    favorite[i].addEventListener('click', function () {
+        if (this.classList.toggle('button.active')) {
+            number++;
+        } else {
+            number--;
+        }
+        document.querySelector('header div').innerHTML = number;
+        this.classList.toggle('active');
+    })
+}
 
 var downloads = document.querySelectorAll('.downloaden');
 for (var i = 0; i < downloads.length; i++) {
     downloads[i].addEventListener('click', function () {
-        this.classList.toggle('active');
-    })
+        this.classList.toggle('active')
+    });
 }
